d23a3faea49c2acb1f3be1549f1c1503338a7a0c
Qianfan Zhang <demnzhang@gmail.com>
2020-11-24 21:01:15 +0800

    cleanup

--- run_all.sh
+++ run_all.sh
@@ -2,14 +2,15 @@
 
 ./build.sh || exit 1
 
-num_samples=$1
-resolution=$2
-
 run() {
     echo
     scene_name=$1
     bin/BINART $scene_name $num_samples $resolution 2>&1 | tee output/$scene_name.log
 }
+
+mkdir -p output
+num_samples=$1
+resolution=$2
 
 run earth_in_box
 run dragon_in_box
