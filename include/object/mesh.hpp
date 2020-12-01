6b5159a8cbe5c4da7a613651e559a6ee8c49a04e
Qianfan Zhang <demnzhang@gmail.com>
2020-12-01 12:53:27 +0800

    use tinyobjloader to load .obj files

--- include/object/mesh.hpp
+++ include/object/mesh.hpp
@@ -9,7 +9,7 @@ class Triangle;
 
 class Mesh : public Object3D {
 public:
-    Mesh(const char *filename, Material *m, Matrix4f transform = Matrix4f::identity(), bool vec_normal = false);
+    Mesh(const char *filename, Material *fallback_material, Matrix4f transform = Matrix4f::identity(), bool use_vec_normal = false);
 
     std::vector<Object3D *> getBasicObjects() override;
 
@@ -21,23 +21,23 @@ public:
 
     std::string getString() const override {
         char buff[100];
-        snprintf(buff, sizeof(buff), "Mesh(%llx,%lu)", (long long)this, t.size());
+        snprintf(buff, sizeof(buff), "Mesh(%llx,%lu)", (long long)this, triangles.size());
         return buff;
     }
 
-    std::vector<Vector3f> v;
-    std::vector<Triangle> t;
-    std::vector<Vector3f> n;
-    std::vector<Vector2f> uv;
-    std::vector<Vector3f> vec_n;
-    bool vec_normal;
+    std::vector<Vector3f> verticies;
+    std::vector<Triangle> triangles;
+    std::vector<Vector3f> tri_normals;
+    std::vector<Vector2f> texcoords;
+    std::vector<Vector3f> vec_normals;
+    bool use_vec_normal;
 };
 
 class Triangle : public Object3D {
 public:
     Triangle(Mesh *mesh) : mesh(mesh) {
         _v[0] = _v[1] = _v[2] = 0;
-        _uv[0] = _uv[1] = _uv[2] = 0;
+        _t[0] = _t[1] = _t[2] = 0;
         _n[0] = _n[1] = _n[2] = 0;
         id = 0;
     }
@@ -47,7 +47,7 @@ public:
     }
 
     BoundingBox getBoundingBox() const override {
-        return BoundingBox(mesh->v[_v[0]], mesh->v[_v[1]]) + mesh->v[_v[2]];
+        return BoundingBox(mesh->verticies[_v[0]], mesh->verticies[_v[1]]) + mesh->verticies[_v[2]];
     }
 
     int &operator[](int i) { return _v[i]; }
@@ -61,7 +61,7 @@ public:
     }
 
     int _v[3];
-    int _uv[3];
+    int _t[3];
     int _n[3];
     int id; // normal id
     Mesh *mesh;
--- src/mesh.cpp
+++ src/mesh.cpp
@@ -1,4 +1,7 @@
+#define TINYOBJLOADER_IMPLEMENTATION
+
 #include "object/mesh.hpp"
+#include "tiny_obj_loader.hpp"
 #include "utils.hpp"
 #include "vecmath.h"
 #include <algorithm>
@@ -11,15 +14,15 @@
 static const float EPS = 1e-6;
 
 bool Triangle::intersect(const Ray &r, Hit &h, float tmin) {
-    Vector3f ab = mesh->v[_v[1]] - mesh->v[_v[0]];
-    Vector3f ac = mesh->v[_v[2]] - mesh->v[_v[0]];
+    Vector3f ab = mesh->verticies[_v[1]] - mesh->verticies[_v[0]];
+    Vector3f ac = mesh->verticies[_v[2]] - mesh->verticies[_v[0]];
     Vector3f p = Vector3f::cross(r.getDirection(), ac);
     float det = Vector3f::dot(ab, p);
     if (std::abs(det) < EPS)
         return false;
 
     float invDet = 1 / det;
-    Vector3f tv = r.getOrigin() - mesh->v[_v[0]];
+    Vector3f tv = r.getOrigin() - mesh->verticies[_v[0]];
     float u = Vector3f::dot(tv, p) * invDet;
     if (u < 0 || u > 1)
         return false;
@@ -31,9 +34,9 @@ bool Triangle::intersect(const Ray &r, Hit &h, float tmin) {
     float t = Vector3f::dot(ac, q) * invDet;
 
     if (t >= tmin && t < h.getT()) {
-        Vector2f uvcoord = (1 - u - v) * mesh->uv[_uv[0]] + u * mesh->uv[_uv[1]] + v * mesh->uv[_uv[2]];
-        Vector3f vec_n = mesh->vec_normal && _n[0] ? (1 - u - v) * mesh->vec_n[_n[0]] + u * mesh->vec_n[_n[1]] + v * mesh->vec_n[_n[2]] : mesh->n[id];
-        h.set(t, this, uvcoord, vec_n);
+        Vector2f texcoord = (1 - u - v) * mesh->texcoords[_t[0]] + u * mesh->texcoords[_t[1]] + v * mesh->texcoords[_t[2]];
+        Vector3f normal = (1 - u - v) * mesh->vec_normals[_n[0]] + u * mesh->vec_normals[_n[1]] + v * mesh->vec_normals[_n[2]] + mesh->tri_normals[id];
+        h.set(t, this, texcoord, normal);
         return true;
     }
     return false;
@@ -41,123 +44,94 @@ bool Triangle::intersect(const Ray &r, Hit &h, float tmin) {
 
 std::vector<Object3D *> Mesh::getBasicObjects() {
     std::vector<Object3D *> objs;
-    for (auto &triangle : t)
+    for (auto &triangle : triangles)
         objs.push_back(&triangle);
     return objs;
 }
 
 bool Mesh::intersect(const Ray &r, Hit &h, float tmin) {
     bool result = false;
-    for (int triId = 0; triId < t.size(); ++triId) {
-        Triangle &triangle = t[triId];
+    for (auto &triangle : triangles)
         result |= triangle.intersect(r, h, tmin);
-    }
     return result;
 }
 
-Mesh::Mesh(const char *filename, Material *material, Matrix4f transform, bool vec_normal)
-    : Object3D(material), vec_normal(vec_normal) {
-    std::ifstream f;
-    f.open(filename);
-    if (!f.is_open()) {
-        std::cout << "Cannot open " << filename << "\n";
-        return;
+Mesh::Mesh(const char *filename, Material *fallback_material, Matrix4f transform, bool use_vec_normal)
+    : Object3D(fallback_material), use_vec_normal(use_vec_normal) {
+    tinyobj::attrib_t attrib;
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
+
+    std::string err_msg;
+    bool load_success = tinyobj::LoadObj(&attrib, &shapes, &materials, &err_msg, filename);
+
+    if (!err_msg.empty())
+        printf("[Mesh] error message: %s\n", err_msg.c_str());
+    if (!load_success) {
+        printf("[Mesh] failed loading obj file %s, aborting\n", filename);
+        fflush(stdout);
+        abort();
     }
-    std::string line;
-    std::string vTok("v");
-    std::string fTok("f");
-    std::string texTok("vt");
-    std::string norTok("vn");
-    std::string groupTok("g");
-    std::string smoothTok("s");
-    char bslash = '/', space = ' ';
-    std::string tok;
 
-    v.push_back(Vector3f::ZERO);
-    uv.push_back(Vector2f::ZERO);
-    n.push_back(Vector3f::ZERO);
+    assert(attrib.vertices.size() % 3 == 0);
+    verticies.resize(attrib.vertices.size() / 3 + 1);
+    for (int v = 0; v < verticies.size() - 1; ++v) {
+        verticies[v + 1][0] = attrib.vertices[3 * v + 0];
+        verticies[v + 1][1] = attrib.vertices[3 * v + 1];
+        verticies[v + 1][2] = attrib.vertices[3 * v + 2];
+        verticies[v + 1] = Utils::transformPoint(transform, verticies[v + 1]);
+    }
 
-    while (true) {
-        std::getline(f, line);
-        if (f.eof()) {
-            break;
-        }
-        if (line.size() < 3) {
-            continue;
-        }
-        if (line.at(0) == '#') {
-            continue;
-        }
+    assert(attrib.normals.size() % 3 == 0);
+    vec_normals.resize(attrib.normals.size() / 3 + 1);
+    for (int n = 0; n < vec_normals.size() - 1; ++n) {
+        vec_normals[n + 1][0] = attrib.normals[3 * n + 0];
+        vec_normals[n + 1][1] = attrib.normals[3 * n + 1];
+        vec_normals[n + 1][2] = attrib.normals[3 * n + 2];
+        vec_normals[n + 1] = Utils::transformNormal(transform, vec_normals[n + 1]);
+    }
 
-        std::stringstream ss(line);
-        ss >> tok;
-        if (tok == vTok) {
-            Vector3f vec;
-            ss >> vec[0] >> vec[1] >> vec[2];
-            v.push_back(Utils::transformPoint(transform, vec));
-        } else if (tok == fTok) {
-            if (line.find(bslash) != std::string::npos) {
-                std::replace(line.begin(), line.end(), bslash, space);
-                std::stringstream ss(line);
-                Triangle trig(this);
-                ss >> tok;
-                for (int ii = 0; ii < 3; ++ii) {
-                    ss >> trig._v[ii] >> trig._uv[ii] >> trig._n[ii];
-                }
-                t.push_back(trig);
-                Triangle trig2 = trig;
-                ss >> trig2._v[2] >> trig2._uv[2] >> trig2._n[2];
-                if (!ss.fail()) {
-                    t.push_back(trig2);
-                    int tmp;
-                    ss >> tmp;
-                    assert(ss.fail()); // maximum 4 verticies
-                }
+    assert(attrib.texcoords.size() % 2 == 0);
+    texcoords.resize(attrib.texcoords.size() / 2 + 1);
+    for (int t = 0; t < texcoords.size() - 1; ++t) {
+        texcoords[t + 1][0] = attrib.texcoords[2 * t + 0];
+        texcoords[t + 1][1] = attrib.texcoords[2 * t + 1];
+    }
 
-            } else {
-                Triangle trig(this);
-                for (int ii = 0; ii < 3; ++ii) {
-                    ss >> trig[ii];
-                }
-                t.push_back(trig);
-                Triangle trig2 = trig;
-                ss >> trig2._v[2];
-                if (!ss.fail()) {
-                    t.push_back(trig2);
-                    int tmp;
-                    ss >> tmp;
-                    assert(ss.fail()); // maximum 4 verticies
-                }
+    for (auto &shape : shapes) {
+        int index_offset = 0;
+        for (int f = 0; f < shape.mesh.num_face_vertices.size(); ++f) {
+            int fv = shape.mesh.num_face_vertices[f];
+            assert(fv == 3);
+
+            // tinyobj::material_t &material = materials[shapes[s].mesh.material_ids[f]];
+            Triangle triangle(this);
+            for (int i = 0; i < fv; ++i) {
+                triangle._v[i] = shape.mesh.indices[index_offset + i].vertex_index + 1;
+                triangle._n[i] = shape.mesh.indices[index_offset + i].normal_index + 1;
+                triangle._t[i] = shape.mesh.indices[index_offset + i].texcoord_index + 1;
+                assert(triangle._v[i] >= 0 && triangle._v[i] < verticies.size());
+                assert(triangle._n[i] >= 0 && triangle._n[i] < vec_normals.size());
+                assert(triangle._t[i] >= 0 && triangle._t[i] < texcoords.size());
             }
-        } else if (tok == texTok) {
-            Vector2f texcoord;
-            ss >> texcoord[0];
-            ss >> texcoord[1];
-            uv.push_back(texcoord);
-        } else if (tok == norTok) {
-            Vector3f norcoord;
-            ss >> norcoord[0];
-            ss >> norcoord[1];
-            ss >> norcoord[2];
-            vec_n.push_back(Utils::transformNormal(transform, norcoord));
-        } else if (tok == groupTok) {
+            triangles.push_back(triangle);
 
-        } else if (tok == smoothTok) {
-
-        } else {
-            std::cerr << "unrecognized line" << std::endl;
-            std::cout << "\t" << line << std::endl;
-            abort();
+            index_offset += fv;
         }
     }
-    f.close();
 
-    n.resize(t.size());
-    for (int triId = 0; triId < (int)t.size(); ++triId) {
-        Triangle &triangle = t[triId];
-        Vector3f a = v[triangle[1]] - v[triangle[0]];
-        Vector3f b = v[triangle[2]] - v[triangle[0]];
-        n[triId] = Vector3f::cross(a, b).normalized();
-        triangle.id = triId;
+    if (!use_vec_normal) {
+        tri_normals.resize(triangles.size());
+        for (int i = 0; i < triangles.size(); ++i) {
+            Triangle &triangle = triangles[i];
+            Vector3f a = verticies[triangle[1]] - verticies[triangle[0]];
+            Vector3f b = verticies[triangle[2]] - verticies[triangle[0]];
+            tri_normals[i] = Vector3f::cross(a, b).normalized();
+            triangle.id = i;
+        }
+    } else {
+        tri_normals.push_back(Vector3f::ZERO);
+        for (auto &triangle : triangles)
+            triangle.id = 0;
     }
 }
